ab9df87c3394dfc062ac305732b7fd7fb9105e9a
SuiPing <kam.suipingooi@me.com>
2021-04-18 07:26:35 +0000

    completed crud for spaces app

--- spaces/urls.py
+++ spaces/urls.py
@@ -3,5 +3,7 @@ import spaces.views
 
 urlpatterns = [
     path('', spaces.views.index, name='bookspace'),
-    path('add', spaces.views.add_space),
+    path('add', spaces.views.add_space, name='addspace'),
+    path('update/<space_id>', spaces.views.update_space, name='updatespace'),
+    path('delete/<space_id>', spaces.views.delete_space, name='deletespace')
 ]
--- spaces/views.py
+++ spaces/views.py
@@ -23,6 +23,35 @@ def add_space(request):
         pass
     else:
         space_form = SpaceForm()
-        return render(request, 'spaces/addspace-template.html', {
+        return render(request, 'spaces/space_add-template.html', {
             'form': space_form
+        })
+
+
+def update_space(request, space_id):
+    space_to_update = get_object_or_404(Space, pk=space_id)
+    if request.method == "POST":
+        space_form = SpaceForm(request.POST, instance=space_to_update)
+        if space_form.is_valid():
+            space_form.save()
+            return redirect(reverse(index))
+        else:
+            return render(request, 'spaces/space_update-template.html', {
+                'form': space_form
+            })
+    else:
+        space_form = SpaceForm(instance=space_to_update)
+        return render(request, 'spaces/space_update-template.html', {
+            'form': space_form
+        })
+
+
+def delete_space(request, space_id):
+    space_to_delete = get_object_or_404(Space, pk=space_id)
+    if request.method == "POST":
+        space_to_delete.delete()
+        return redirect(index)
+    else:
+        return render(request, 'spaces/space_delete-template.html', {
+            'space': space_to_delete
         })
