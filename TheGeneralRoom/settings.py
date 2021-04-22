dbea2fd9ef6ce19afaccb78b512a7d6d4b39129f
SuiPing <kam.suipingooi@me.com>
2021-04-22 15:33:42 +0000

    .env gitignore, uploadcare settings .env

--- TheGeneralRoom/settings.py
+++ TheGeneralRoom/settings.py
@@ -1,11 +1,18 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
+UPLOADCARE = {
+    'pub_key': os.environ.get('UPLOADCARE_PUBLIC_KEY'),
+    'secret': os.environ.get('UPLOADCARE_SECRET_KEY')
+}
+print(UPLOADCARE)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
