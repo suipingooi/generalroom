fe40ab94ed96a185cdd8078d8467900ed801d4ec
SuiPing <kam.suipingooi@me.com>
2021-05-09 13:52:21 +0000

    updated database to heroku-postgres

--- TheGeneralRoom/settings.py
+++ TheGeneralRoom/settings.py
@@ -1,6 +1,7 @@
+import os
 from pathlib import Path
 from dotenv import load_dotenv
-import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,11 +117,15 @@ WSGI_APPLICATION = 'TheGeneralRoom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
